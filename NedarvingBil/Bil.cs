c98b6691c33beadfe8e1661069d0b9f06414b0bc
anne55x9 <anne55x9@edu.easj.dk>
2017-02-06 14:46:35 +0100

    opgave 1 er forsøgt med nested if som afgiftudregning for nyBIl

--- NedarvingBil/Bil.cs
+++ NedarvingBil/Bil.cs
@@ -8,17 +8,25 @@ namespace NedarvingBil
 {
     public abstract class Bil
     {
-        public int pris { get; private set; }
+        public int BilPrisExAfgift { get; private set; }
+
+        public int KøbsÅr { get; private set; }
+
+        public string Mærke { get; set; }
+
+        public string RegistreringsNr { get; set; }
+
+        public int KmPrLiter { get; set; }
 
         /// <summary>
         /// ingen default ctor længere
         /// </summary>
         /// <param name="pris"></param>
 
-        public Bil(int pris)
-        {
-            this.pris = pris;
-        }
+        //public Bil(int pris)
+        //{
+        //    this.pris = pris;
+        //}
 
         /// <summary>
         /// Afgift må gerne blive overskrevet, behøves ikke men kan godt, deraf virtual navnet
@@ -30,6 +38,56 @@ namespace NedarvingBil
         //    return 1000; 
         //}
 
-        public abstract int Afgift();
+        public Bil(string mærke, int bilPrisExAfgift, int købsÅr, int kmPrLiter)
+        {
+            this.Mærke = mærke;
+            this.BilPrisExAfgift = bilPrisExAfgift;
+            this.KøbsÅr = købsÅr;
+            this.KmPrLiter = KmPrLiter;
+        }
+
+        public virtual int RegistreringAfgift()
+        {
+            int pris1 = 0;
+            int pris2 = 0;
+            if (KøbsÅr <= 2014)
+            {
+                if (BilPrisExAfgift <= 80500)
+                {
+                   pris1 = BilPrisExAfgift * (105 / 100);
+                   return pris1;
+                }
+                else
+                {
+                   pris2 = (BilPrisExAfgift - 80500) * (180 / 100);
+                   return pris1 + pris2;
+                }
+            }
+            else
+            {
+                if (BilPrisExAfgift <= 81700)
+                {
+                    pris1 = BilPrisExAfgift * (105 / 100);
+                    return pris1;
+                }
+                else
+                {
+                    pris2 = (BilPrisExAfgift -81700) * (180/100);
+                    return pris1 + pris2;
+                }
+            }
+        }
+
+        public int TotalPris()
+        {
+           return BilPrisExAfgift + RegistreringAfgift();
+        }
+
+        public virtual int HalvÅrligEjerafgift()
+        {
+            return 1000;
+        }
+
+
     }
 }
