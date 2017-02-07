dc7ac8b7ed5d1cab110bec636c9e492f59bc3a75
anne55x9 <anne55x9@edu.easj.dk>
2017-02-07 15:02:59 +0100

    Extracted interface via quick launch med curser i bil klassen. IBil interfacen er trukket ud af bil klassen og ligger nu som cs fil i solution folder.

---
+++ NedarvingBil/IBil.cs
@@ -0,0 +1,17 @@
+﻿namespace NedarvingBil
+{
+    public interface IBil
+    {
+        int BilPrisExAfgift { get; }
+        int KøbsÅr { get; }
+        string Mærke { get; set; }
+        int RegistreringsAfgift { get; set; }
+        string RegistreringsNr { get; set; }
+
+        int GetRegistreringsAfgift();
+        int GetRækkeVidde();
+        int HalvÅrligEjerafgift();
+        string HvilkenBilErJeg();
+        int TotalPris();
+    }
+}
