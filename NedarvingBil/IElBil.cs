38117d9b04c16331c46345c23798e51817fb1cc5
anne55x9 <anne55x9@edu.easj.dk>
2017-02-07 15:28:04 +0100

    Elbil fil slettet ved fejr, er genskabt.

---
+++ NedarvingBil/IElBil.cs
@@ -0,0 +1,15 @@
+﻿namespace NedarvingBil
+{
+    public interface IElBil
+    {
+        int BatteriKapacitet { get; set; }
+        int KmPrKw { get; set; }
+        int LadeTid { get; set; }
+
+        int GetRegistreringsAfgift();
+        int GetRækkeVidde();
+        int HalvÅrligEjerafgift();
+        string HvilkenBilErJeg();
+        string ToString();
+    }
+}
