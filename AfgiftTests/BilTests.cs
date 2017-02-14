fb7ed8ff22f1860fc00a99922df6e5578b540847
Søren Roed Schack <soer622t@edu.easj.dk>
2017-02-14 11:20:33 +0100

    Første git af Solution inkl. Unittest.

--- AfgiftTests/BilTests.cs
+++ AfgiftTests/BilTests.cs
@@ -12,20 +12,147 @@ namespace NedarvingBil.Tests
 
     public class BilTests
     {
+        /// <summary>
+        /// Test af Registreringsafgift på Dieselbiler, Årgang 2014
+        /// </summary>
         [TestMethod()]
         //[ExpectedException(typeof(ArgumentException))]
+        public void GetRegistreringsAfgiftTest_DieselBil2014_5000cost()
+        {
+            //arrange
+            DieselBil dieselbil = new DieselBil("Tesla", 5000, 2014, "DD77380", 60, true, 10);
+
+            //act
+            int afgift = dieselbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(5250, afgift);
+        }
+
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_DieselBil2014_80499cost()
+        {
+            //arrange
+            DieselBil dieselbil = new DieselBil("Tesla", 80499, 2014, "DD77380", 60, true, 10);
+
+            //act
+            int afgift = dieselbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(84523, afgift);
+        }
+
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_DieselBil2014_80500cost()
+        {
+            //arrange
+            DieselBil dieselbil = new DieselBil("Tesla", 80500, 2014, "DD77380", 60, true, 10);
+
+            //act
+            int afgift = dieselbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(84525, afgift);
+        }
+
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_DieselBil2014_80501cost()
+        {
+            //arrange
+            DieselBil dieselbil = new DieselBil("Tesla", 80501, 2014, "DD77380", 60, true, 10);
+
+            //act
+            int afgift = dieselbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(84526, afgift);
+        }
+
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_DieselBil2014_100000cost()
+        {
+            //arrange
+            DieselBil dieselbil = new DieselBil("Tesla", 100000, 2014, "DD77380", 60, true, 10);
+
+            //act
+            int afgift = dieselbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(119625,afgift);
+        }
+
+        /// <summary>
+        /// Test af Registreringsafgift på Dieselbiler, Årgang 2015
+        /// </summary>
+        /// 
+        [TestMethod()]
+        //[ExpectedException(typeof(ArgumentException))]
+        public void GetRegistreringsAfgiftTest_DieselBil2015_5000cost()
+        {
+            //arrange
+            DieselBil dieselbil = new DieselBil("Tesla", 5000, 2015, "DD77380", 60, true, 10);
+
+            //act
+            int afgift = dieselbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(5250, afgift);
+        }
+
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_DieselBil2015_81699cost()
+        {
+            //arrange
+            DieselBil dieselbil = new DieselBil("Tesla", 81699, 2015, "DD77380", 60, true, 10);
+
+            //act
+            int afgift = dieselbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(85783, afgift);
+        }
+
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_DieselBil2015_81700cost()
+        {
+            //arrange
+            DieselBil dieselbil = new DieselBil("Tesla", 81700, 2015, "DD77380", 60, true, 10);
+
+            //act
+            int afgift = dieselbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(85785, afgift);
+        }
+
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_DieselBil2015_81701cost()
+        {
+            //arrange
+            DieselBil dieselbil = new DieselBil("Tesla", 81701, 2015, "DD77380", 60, true, 10);
+
+            //act
+            int afgift = dieselbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(85786, afgift);
+        }
+
+        [TestMethod()]
         public void GetRegistreringsAfgiftTest_DieselBil2015_100000cost()
         {
             //arrange
             DieselBil dieselbil = new DieselBil("Tesla", 100000, 2015, "DD77380", 60, true, 10);
 
+            //act
             int afgift = dieselbil.GetRegistreringsAfgift();
 
             //assert
-
-            Assert.AreEqual(118725,afgift);
-
-           // Assert.Fail();
+            Assert.AreEqual(118725, afgift);
         }
+
+        /// <summary>
+        /// Mangler at teste Registreringsafgift på Benzinbiler, Årgang 2014 og 2015
+        /// </summary>
     }
 }
---
+++ AfgiftTestsElbil1/ElBilTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NedarvingBil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NedarvingBil.Tests
+{
+    [TestClass()]
+    public class ElBilTests
+    {
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_Elbil2014_5000kr()
+        {
+            //arrange
+            ElBil elbil = new ElBil("Nissan Leaf", 5000, 2014, "EL201401", 8, 30);
+
+            //act
+            int afgiftElbil = elbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(1050, afgiftElbil);
+        }
+
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_Elbil2014_80499kr()
+        {
+            //arrange
+            ElBil elbil = new ElBil("Nissan Leaf", 80499, 2014, "EL201402", 8, 30);
+
+            //act
+            int afgiftElbil = elbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(16904, afgiftElbil);
+        }
+
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_Elbil2014_80500kr()
+        {
+            //arrange
+            ElBil elbil = new ElBil("Nissan Leaf", 80500, 2014, "EL201402", 8, 30);
+
+            //act
+            int afgiftElbil = elbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(16905, afgiftElbil);
+        }
+
+        [TestMethod()]
+        public void GetRegistreringsAfgiftTest_Elbil2014_80501kr()
+        {
+            //arrange
+            ElBil elbil = new ElBil("Nissan Leaf", 80501, 2014, "EL201402", 8, 30);
+
+            //act
+            int afgiftElbil = elbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(16905, afgiftElbil);
+        }
+
+        public void GetRegistreringsAfgiftTest_Elbil2014_100000kr()
+        {
+            //arrange
+            ElBil elbil = new ElBil("Nissan Leaf", 100000, 2014, "EL201402", 8, 30);
+
+            //act
+            int afgiftElbil = elbil.GetRegistreringsAfgift();
+
+            //assert
+            Assert.AreEqual(23925, afgiftElbil);
+        }
+
+        // Mangler at teste elbiler årgang 2015
+    }
+}
